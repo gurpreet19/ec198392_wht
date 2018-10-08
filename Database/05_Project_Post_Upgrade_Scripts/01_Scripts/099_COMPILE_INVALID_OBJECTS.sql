ccb673065b44024f7d757919aca02dddd01f7352
desaiadi <desaiadi@localhost>
2018-10-08 13:42:39 +0530

    Updated Post Compile Script

--- Database/05_Project_Post_Upgrade_Scripts/01_Scripts/099_COMPILE_INVALID_OBJECTS.sql
+++ Database/05_Project_Post_Upgrade_Scripts/01_Scripts/099_COMPILE_INVALID_OBJECTS.sql
@@ -1,9 +1,3 @@
----running force buildviewlayer on all objects
-update VIEWLAYER_DIRTY_LOG set dirty_ind='Y';
-commit;
-
-execute ecdp_viewlayer.buildviewlayer(p_force => 'Y'); 
-execute ecdp_viewlayer.buildreportlayer(p_force => 'Y'); 
 ---refresh materialized views
 execute ecdp_classmeta.RefreshMViews(p_force => 'Y');
 --EXECUTE ec_generate.Synchronise;
