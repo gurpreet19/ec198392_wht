a8a5babd09f5d581bd854852fe84e5bf83d7f652
subrasun3 <sundarapandian.subramanian@tieto.com>
2019-04-08 17:09:40 +0530

    compile ecdp_synchronise

--- Database/05_Project_Post_Upgrade_Scripts/01_Scripts/099_COMPILE_INVALID_OBJECTS.sql
+++ Database/05_Project_Post_Upgrade_Scripts/01_Scripts/099_COMPILE_INVALID_OBJECTS.sql
@@ -1,14 +1,8 @@
----running force buildviewlayer on all objects
-update VIEWLAYER_DIRTY_LOG set dirty_ind='Y';
-commit;
-
-execute ecdp_viewlayer.buildviewlayer(p_force => 'Y'); 
-execute ecdp_viewlayer.buildreportlayer(p_force => 'Y'); 
+---running build view and report layer
+exec ecdp_viewlayer.buildviewlayer(p_force => 'Y'); 
+exec ecdp_viewlayer.buildreportlayer(p_force => 'Y'); 
 ---refresh materialized views
-execute ecdp_classmeta.RefreshMViews(p_force => 'Y');
---EXECUTE ec_generate.Synchronise;
- 
----t_temptext trunctate
-DELETE FROM t_temptext WHERE id LIKE 'INVALIDOBJECTS%';
+EXECUTE ECDP_SYNCHRONISE.Synchronise;
+commit;
  
 execute dbms_utility.compile_schema(user,false);
