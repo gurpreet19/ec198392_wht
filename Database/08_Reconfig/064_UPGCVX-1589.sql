d7c4e86a52ac0a006c2f53a5bc24d4824cf72647
kulkagay <kulkagay@localhost>
2019-05-21 17:07:54 +0530

    Defect fixes

---
+++ Database/08_Reconfig/064_UPGCVX-1589.sql
@@ -0,0 +1,2 @@
+update prosty_codes set alt_code='/com.ec.wheatstone.screens/layout/cond_demurrage.xml' where code='COND_DEMURRAGE';
+update prosty_codes set alt_code='/com.ec.wheatstone.screens/layout/cond_ebo' where code=COND_EBO;
