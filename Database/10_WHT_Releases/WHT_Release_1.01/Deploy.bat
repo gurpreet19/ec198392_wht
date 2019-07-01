7ab205886053a6a0725509b93c0154d622c63cf2
kulkagay <kulkagay@localhost>
2019-07-01 16:46:34 +0530

    Added release folder and script for missing attributes

---
+++ Database/10_WHT_Releases/WHT_Release_1.01/Deploy.bat
@@ -0,0 +1,40 @@
+@echo off
+Echo Started Deploy script..
+
+set homepath=%cd%
+
+
+
+Echo **************************************************
+Echo Running CDB release 1 scripts..
+Echo **************************************************
+
+Echo ****************************************************
+Echo Running CDB release 1 database defect scripts
+Echo ****************************************************
+
+sqlplus /nolog @ InstallScript.sql
+if errorlevel 1 (
+      goto error 
+				)
+				
+		
+Echo ****************************************************
+Echo Execution Completed for database defect scripts
+Echo ****************************************************
+
+Echo ****************************************************
+Echo Execution Completed CDB release 1 scripts..
+Echo ****************************************************
+
+				
+cd %homepath%
+
+:: no error, skip error message
+goto end
+:error
+echo error occurred - check log file
+cd %homepath%
+:end
+
+cd %homepath%
