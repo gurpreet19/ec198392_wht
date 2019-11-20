715f30fb3612e0c112b674096cba1fe5dc01f9be
dhavaalo <dhavaalo@localhost>
2019-11-20 17:01:46 +0530

    release call added

--- Database/1_Upgrade.bat
+++ Database/1_Upgrade.bat
@@ -1,2 +1,33 @@
 @echo off
-_Perl\bin\perl.exe "_Perl\upgrade.pl" 1 1,2,3,4,5,6,7,8,9,10,11,13,14,15
+REM _Perl\bin\perl.exe "_Perl\upgrade.pl" 1 1,2,3,4,5,6,7,8,9,10,11,13,14,15
+
+cd "10_WHT_Releases\WHT_Release_1.01"
+call "BuildScript.bat"
+call "Deploy.bat"
+cd..
+cd..
+cd "10_WHT_Releases\WHT_Release_1.02"
+call "BuildScript.bat"
+call "Deploy.bat"
+cd..
+cd..
+cd "10_WHT_Releases\WHT_Release_1.03"
+call "BuildScript.bat"
+call "Deploy.bat"
+cd..
+cd..
+cd "10_WHT_Releases\WHT_Release_1.04"
+call "BuildScript.bat"
+call "Deploy.bat"
+cd..
+cd..
+cd "10_WHT_Releases\WHT_Release_1.05"
+call "BuildScript.bat"
+call "Deploy.bat"
+cd..
+cd..
+cd "10_WHT_Releases\WHT_Release_1.06"
+call "BuildScript.bat"
+call "Deploy.bat"
+cd..
+cd..
