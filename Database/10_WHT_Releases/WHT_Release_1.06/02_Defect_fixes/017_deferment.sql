8e03e89c1e74948a5335a959193fb8de2c635cc8
agarwnik <agarwnik@localhost>
2019-12-03 11:27:43 +0530

    Update class_name

--- Database/10_WHT_Releases/WHT_Release_1.06/02_Defect_fixes/017_deferment.sql
+++ Database/10_WHT_Releases/WHT_Release_1.06/02_Defect_fixes/017_deferment.sql
@@ -1,8 +1,8 @@
 insert into class_attr_property_cnfg (CLASS_NAME, ATTRIBUTE_NAME, PROPERTY_CODE, OWNER_CNTX, PRESENTATION_CNTX, PROPERTY_TYPE, PROPERTY_VALUE)
 values ('WELL_DEFERMENT',	'REASON_CODE_3',	'IS_MANDATORY',	2500	,'/',	'VIEWLAYER',	'N');
 
-Update viewlayer_dirty_log set dirty_ind = 'Y' where object_name IN( 'CT_LPO_OFF_EVENT_DAY');
+Update viewlayer_dirty_log set dirty_ind = 'Y' where object_name IN( 'WELL_DEFERMENT');
 commit;
 
-execute ecdp_viewlayer.buildviewlayer('CT_LPO_OFF_EVENT_DAY', p_force => 'Y'); 
-execute ecdp_viewlayer.buildreportlayer('CT_LPO_OFF_EVENT_DAY', p_force => 'Y'); 
+execute ecdp_viewlayer.buildviewlayer('WELL_DEFERMENT', p_force => 'Y'); 
+execute ecdp_viewlayer.buildreportlayer('WELL_DEFERMENT', p_force => 'Y'); 
