873b95a10f197f1410cf2e3e945216477fd80994
dhavaalo <dhavaalo@localhost>
2019-12-05 09:46:19 +0530

    new bat added

---
+++ Database/2_Upgrade.bat
@@ -0,0 +1,8 @@
+@echo off
+REM _Perl\bin\perl.exe "_Perl\upgrade.pl" 1 1,2,3,4,5,6,7,8,9,10,11,13,14,15
+
+cd "10_WHT_Releases\WHT_Release_1.07"
+call "BuildScript.bat"
+call "Deploy.bat"
+cd..
+cd..
