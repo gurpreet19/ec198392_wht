f87aa8046ab25f5c717794200e476d8740ac7c59
dhavaalo <dhavaalo@localhost>
2019-12-13 13:50:51 +0530

    Re-modified

--- Database/10_WHT_Releases/WHT_Release_1.07/02_Defect_fixes/011_UPGCVX-2452.sql
+++ Database/10_WHT_Releases/WHT_Release_1.07/02_Defect_fixes/011_UPGCVX-2452.sql
@@ -1,14 +1,12 @@
-insert into class_attr_property_cnfg (class_name, attribute_name, property_code, owner_cntx, presentation_cntx, property_type, property_value)
-values('IWEL_DAY_STATUS_STEAM', 'INJ_VOL_2', 'viewlabelhead', '2500', '/EC', 'STATIC_PRESENTATION', 'Injected Steam');
+insert into class_attr_property_cnfg (class_name, attribute_name, property_code, owner_cntx, presentation_cntx, property_type, property_value) values('IWEL_DAY_STATUS_STEAM', 'INJ_VOL_2', 'viewlabelhead', '2500', '/EC', 'STATIC_PRESENTATION', 'Injected Steam');
 
-update class_attr_property_cnfg
-set property_value = '710', OWNER_CNTX = '2500' where class_name = 'IWEL_DAY_STATUS_STEAM' and attribute_name = 'INJ_VOL_2' and property_code = 'SCREEN_SORT_ORDER';
+Insert into CLASS_ATTR_PROPERTY_CNFG (CLASS_NAME,ATTRIBUTE_NAME,PROPERTY_CODE,OWNER_CNTX,PRESENTATION_CNTX,PROPERTY_TYPE,PROPERTY_VALUE) values ('IWEL_DAY_STATUS_STEAM','INJ_VOL_2','SCREEN_SORT_ORDER',2500,'/EC','APPLICATION','710');
 
-update class_attr_property_cnfg
-set property_value = '600', OWNER_CNTX = '2500' where class_name = 'IWEL_DAY_STATUS_STEAM' and attribute_name = 'INJ_VOL' and property_code = 'SCREEN_SORT_ORDER';
+Insert into CLASS_ATTR_PROPERTY_CNFG (CLASS_NAME,ATTRIBUTE_NAME,PROPERTY_CODE,OWNER_CNTX,PRESENTATION_CNTX,PROPERTY_TYPE,PROPERTY_VALUE) values ('IWEL_DAY_STATUS_STEAM','INJ_VOL','SCREEN_SORT_ORDER',2500,'/EC','APPLICATION','600');
 
 
 UPDATE viewlayer_dirty_log SET DIRTY_IND='Y' WHERE object_name in ('IWEL_DAY_STATUS_STEAM');
+
 commit;
 EXECUTE EcDp_Viewlayer.BuildViewLayer('IWEL_DAY_STATUS_STEAM',p_force => 'Y');
 EXECUTE EcDp_Viewlayer.BuildReportLayer('IWEL_DAY_STATUS_STEAM',p_force => 'Y');
--- Database/10_WHT_Releases/WHT_Release_1.07/02_Defect_fixes/013_UPGCVX-2445.sql
+++ Database/10_WHT_Releases/WHT_Release_1.07/02_Defect_fixes/013_UPGCVX-2445.sql
@@ -1,17 +1,35 @@
-update class_attr_property_cnfg set property_value = '560' where class_name = 'STRM_DAY_STREAM_MEAS_OIL' and attribute_name = 'ALLOC_NET_VOL' and property_code = 'SCREEN_SORT_ORDER' ;
-update class_attr_property_cnfg set property_value = '565' where class_name = 'STRM_DAY_STREAM_MEAS_OIL' and attribute_name = 'NET_MASS_OIL' and property_code = 'SCREEN_SORT_ORDER' ;
-update class_attr_property_cnfg set property_value = '570' where class_name = 'STRM_DAY_STREAM_MEAS_OIL' and attribute_name = 'ALLOC_OIL_ENERGY' and property_code = 'SCREEN_SORT_ORDER' ;
-UPDATE viewlayer_dirty_log SET DIRTY_IND='Y' WHERE object_name in ('STRM_DAY_STREAM_MEAS_OIL');
-commit;
-EXECUTE EcDp_Viewlayer.BuildViewLayer('STRM_DAY_STREAM_MEAS_OIL',p_force => 'Y') ;
-EXECUTE EcDp_Viewlayer.BuildReportLayer('STRM_DAY_STREAM_MEAS_OIL',p_force => 'Y');
+Insert into CLASS_ATTR_PROPERTY_CNFG (CLASS_NAME,ATTRIBUTE_NAME,PROPERTY_CODE,OWNER_CNTX,PRESENTATION_CNTX,PROPERTY_TYPE,PROPERTY_VALUE) values ('STRM_DAY_STREAM_MEAS_OIL','ALLOC_NET_VOL','SCREEN_SORT_ORDER',2500,'/EC','APPLICATION','2650');
+
+
+update class_attr_property_cnfg
+set property_value = 565
+where class_name = 'STRM_DAY_STREAM_MEAS_OIL' and property_code = 'SCREEN_SORT_ORDER' and attribute_name = 'NET_MASS_OIL'
+and OWNER_CNTX=2500;
+
+update class_attr_property_cnfg
+set property_value = 3200
+where class_name = 'STRM_DAY_STREAM_MEAS_OIL' and property_code = 'SCREEN_SORT_ORDER' and attribute_name = 'ALLOC_OIL_ENERGY'
+and OWNER_CNTX=2500;
+
 
 update class_attr_property_cnfg 
 set property_value = 'Volume'
 where class_name = 'STRM_DAY_STREAM_DER_OIL' and attribute_name = 'ALLOC_NET_VOL' and property_code = 'LABEL' and owner_cntx = '2500';
 
+
+
+UPDATE viewlayer_dirty_log SET DIRTY_IND='Y' WHERE object_name in ('STRM_DAY_STREAM_MEAS_OIL');
+commit;
+EXECUTE EcDp_Viewlayer.BuildViewLayer('STRM_DAY_STREAM_MEAS_OIL',p_force => 'Y') ;
+EXECUTE EcDp_Viewlayer.BuildReportLayer('STRM_DAY_STREAM_MEAS_OIL',p_force => 'Y');
+
 UPDATE viewlayer_dirty_log SET DIRTY_IND='Y' WHERE object_name in ('STRM_DAY_STREAM_DER_OIL');
 commit;
+EXECUTE EcDp_Viewlayer.BuildViewLayer('STRM_DAY_STREAM_DER_OIL',p_force => 'Y') ;
+EXECUTE EcDp_Viewlayer.BuildReportLayer('STRM_DAY_STREAM_DER_OIL',p_force => 'Y');
 
-EXECUTE EcDp_Viewlayer.BuildViewLayer('STRM_DAY_STREAM_DER_OIL ',p_force => 'Y') ;
-EXECUTE EcDp_Viewlayer.BuildReportLayer('STRM_DAY_STREAM_DER_OIL ',p_force => 'Y');
+
+
+
+
+
