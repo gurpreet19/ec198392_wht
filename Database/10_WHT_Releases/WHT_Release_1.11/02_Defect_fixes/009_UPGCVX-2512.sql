4f1db5108e02eff0de92fc98a72a12a33d467f26
dhavaalo <dhavaalo@localhost>
2020-01-16 10:49:20 +0530

    delete added on code field

--- Database/10_WHT_Releases/WHT_Release_1.11/02_Defect_fixes/009_UPGCVX-2512.sql
+++ Database/10_WHT_Releases/WHT_Release_1.11/02_Defect_fixes/009_UPGCVX-2512.sql
@@ -1,13 +1,7 @@
-update ov_company SET OBJECT_END_DATE='15-APR-15' WHERE OBJECT_ID='97B3426B6B7F31DFE0530100007FF750';
+update ov_company SET OBJECT_END_DATE='15-APR-15' WHERE CODE='COMPANY_1';
 
-update ov_company SET OBJECT_END_DATE='14-APR-15' WHERE OBJECT_ID='97B3426B6B8231DFE0530100007FF750';
+update ov_company SET OBJECT_END_DATE='15-APR-15' WHERE CODE='COMPANY_2';
 
-
-commit;
-UPDATE viewlayer_dirty_log SET DIRTY_IND='Y' WHERE object_name in ('ov_company');
-commit;
-EXECUTE EcDp_Viewlayer.BuildViewLayer('ov_company',p_force => 'Y') ;
-EXECUTE EcDp_Viewlayer.BuildReportLayer('ov_company',p_force => 'Y');
 commit;
 
 
