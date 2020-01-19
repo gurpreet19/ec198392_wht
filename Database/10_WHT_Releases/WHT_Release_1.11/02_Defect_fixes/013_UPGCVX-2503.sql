29d1091a5dfccbe52f8b2efa50673b85289ace44
vkkgunner <vkkgunner@localhost>
2020-01-19 15:19:31 +0530

    changing timings as per customer need

--- Database/10_WHT_Releases/WHT_Release_1.11/02_Defect_fixes/013_UPGCVX-2503.sql
+++ Database/10_WHT_Releases/WHT_Release_1.11/02_Defect_fixes/013_UPGCVX-2503.sql
@@ -27,8 +27,8 @@ BEGIN
   BEGIN
   sys.dbms_job.submit(job => x,
                       what => 'ec_Bs_Instantiate.new_day_start(trunc(EcDp_Date_Time.getCurrentSysdate));',
-                      next_date => trunc(sysdate+1),
-                      interval => 'trunc(sysdate+1)',
+                      next_date => trunc(sysdate) + 00/24 + 00/1440,
+                      interval => 'trunc(sysdate) + 1 + 00/24 + 00/1440',
 					  no_parse  => FALSE);
   commit;
     SYS.DBMS_OUTPUT.PUT_LINE('Job Number is: ' || to_char(x));
@@ -70,8 +70,8 @@ BEGIN
   BEGIN
   sys.dbms_job.submit(job => x,
                       what => 'ec_Bs_Instantiate.new_day_end(trunc(EcDp_Date_Time.getCurrentSysdate));',
-                      next_date => trunc(sysdate+1)+23/24 + 50/24/60,
-                      interval => 'trunc(sysdate+1)+23/24 + 50/24/60',
+                      next_date => trunc(sysdate) + 23/24 + 50/1440,
+                      interval => 'TRUNC(sysdate) + 1 + 23/24 + 50/1440'
 					  no_parse  => FALSE);
   commit;
     SYS.DBMS_OUTPUT.PUT_LINE('Job Number is: ' || to_char(x));
@@ -114,7 +114,7 @@ BEGIN
   BEGIN
   sys.dbms_job.submit(job => x,
                       what => 'ue_bs_instantiate.new_day_end(trunc(EcDp_Date_Time.getCurrentSysdate));',
-                      next_date => trunc(sysdate+1)+23/24 + 51/24/60,
+                      next_date => trunc(sysdate) + 23/24 + 51/1440,
                       interval => 'trunc(sysdate+1)+23/24 + 51/24/60',
 					  no_parse  => FALSE);
   commit;
@@ -157,8 +157,8 @@ BEGIN
   BEGIN
   sys.dbms_job.submit(job => x,
                       what => 'UE_CT_BS_INSTANTIATE.new_day_start(trunc(EcDp_Date_Time.getCurrentSysdate));',
-                      next_date => trunc(sysdate+1)+5/24/60,
-                      interval => 'trunc(sysdate+1)+5/24/60',
+                      next_date => trunc(sysdate) + 00/24 + 05/1440,
+                      interval => 'trunc(sysdate) + 1 + 00/24 + 05/1440',
 					  no_parse  => FALSE);
 
   commit;
